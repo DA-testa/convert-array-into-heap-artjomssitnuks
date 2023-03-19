00927ec7a324496f04189ae1ea68265a2dea9db5
artjomssitnuks <125254369+artjomssitnuks@users.noreply.github.com>
2023-03-19 15:40:39 +0200

    Update build_heap.py

--- build_heap.py
+++ build_heap.py
@@ -5,39 +5,27 @@ def build_heap(data):
     swaps = []
     n = len(data)
     for i in range(n // 2 - 1, -1, -1):
-        while True:
-            max_child_idx = 2 * i + 1
-            if max_child_idx + 1 < n and data[max_child_idx + 1] > data[max_child_idx]:
-                max_child_idx += 1
-            if data[i] >= data[max_child_idx]:
+        min_child_idx = 2 * i + 1
+        while min_child_idx < n:
+            if min_child_idx + 1 < n and data[min_child_idx + 1] < data[min_child_idx]:
+                min_child_idx += 1
+            if data[i] <= data[min_child_idx]:
                 break
-            swaps.append((i, max_child_idx))
-            data[i], data[max_child_idx] = data[max_child_idx], data[i]
-            i = max_child_idx
+            swaps.append((i, min_child_idx))
+            data[i], data[min_child_idx] = data[min_child_idx], data[i]
+            i = min_child_idx
+            min_child_idx = 2 * i + 1
     return swaps
 
+
+
 def main():
-    input_opt, n = input().strip().split()
-
-    n = int(n)
-
-    if input_opt == "I":
-        data = list(map(int, input().split()))
-        assert len(data) == n
-    elif input_opt == "F":
-        with open(input(), "r") as f:
-            data = list(map(int, f.readline().split()))
-        assert len(data) == n
-    else:
-        raise ValueError("Invalid input option")
-
+    n = int(input())
+    data = list(map(int, input().split()))
     swaps = build_heap(data)
-    assert len(swaps) < 4
-
     print(len(swaps))
     for i, j in swaps:
         print(i, j)
 
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
